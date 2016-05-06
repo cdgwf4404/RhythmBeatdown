ffbebc3ad2be18485c1f51f7eb651f077927a4f3
Colin G <Colin G>
2016-05-05 18:51:31 -0600

    Visualizer Scripts for Rhythm Beatdown

---
+++ Visualizer_Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 20.0f;
+	public float jumpSpeed = 7.0f;
+	public int maxJump = 2;
+	public int jumpCount = 0;
+	private bool isGrounded = true;
+	private Rigidbody rb;
+	bool rotate = false;
+	public float crouch = 0.5f;
+	
+	public MeshRenderer[] death;
+	public BoxCollider[] death2;
+	public Rigidbody[] death3;
+	/*void Start()
+    {
+        rb = GetComponent<Rigidbody> ();
+    }*/
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.DownArrow)) 
+		{
+			transform.localScale = new Vector3(transform.localScale.x, crouch, transform.localScale.z);
+		}
+		if (Input.GetKeyUp (KeyCode.DownArrow)) 
+		{
+			transform.localScale = new Vector3(transform.localScale.x, 0.75f, transform.localScale.z);
+		}
+		
+		
+		
+		if (Input.GetKeyDown (KeyCode.RightArrow) && rotate == true) 
+		{
+			transform.RotateAround (transform.position, transform.up, 180f);
+			rotate = false;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && rotate == false) 
+		{
+			transform.RotateAround(transform.position, transform.up, 180f);
+			rotate = true;
+		}
+		
+		var move = new Vector3 (Input.GetAxis ("Horizontal"), 0);
+		GetComponent<Rigidbody> ().position += move * speed * Time.deltaTime;
+		
+		if (Input.GetKeyDown (KeyCode.Space) && jumpCount < maxJump) 
+		{
+			GetComponent<Rigidbody> ().velocity += Vector3.up * jumpSpeed;
+			jumpCount++;
+		}
+		if (jumpCount > maxJump || isGrounded == false) 
+		{
+			jumpSpeed = 0.0f;
+		}
+		if (jumpCount > maxJump && isGrounded == true) 
+		{
+			jumpSpeed = 0.0f;
+			jumpCount = 1;
+		}
+		
+	}
+	
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Cubes") 
+		{
+			jumpCount = 0;
+			jumpSpeed = 7.0f;
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (gameObject.tag == "Player") 
+		{
+			//StartCoroutine(respawn());
+		}
+	}
+	
+//	IEnumerator respawn()
+//	{
+//		if (gameObject.tag == "Player") 
+//		{
+//			death = gameObject.GetComponentsInChildren<MeshRenderer> ();
+//			death2 = gameObject.GetComponentsInChildren<BoxCollider> ();
+//			death3 = gameObject.GetComponentsInChildren<Rigidbody> ();
+//			foreach(MeshRenderer mesh in death)
+//				mesh.enabled = false;
+//			foreach(BoxCollider mesh in death2)
+//				mesh.enabled = false;
+//			foreach(Rigidbody mesh in death3)
+//				mesh.useGravity = false;
+//			
+//			yield return new WaitForSeconds(2);
+//			
+//			gameObject.transform.position = (GameObject.FindWithTag ("Spawn").transform.position);
+//			foreach(MeshRenderer mesh in death)
+//				mesh.enabled = true;
+//			foreach(BoxCollider mesh in death2)
+//				mesh.enabled = true;
+//			foreach(Rigidbody mesh in death3)
+//				mesh.useGravity = true;
+//		}
+//	}
+}
---
+++ Visualizer_Test/Assets/Scripts/Spectrum.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spectrum : MonoBehaviour {
+
+	public GameObject cubePrefab;
+	public int numberOfObjects;
+	public GameObject[] cubes;
+	public int spectrumScale = 400;
+	public float cubeSpacing = 2.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cubes = new GameObject[numberOfObjects];
+
+		for (int i = 0; i < numberOfObjects; i++) 
+		{
+			cubes[i] = Instantiate (cubePrefab, new Vector3 (i * cubeSpacing, 0, 0), Quaternion.identity) as GameObject;
+		}
+
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float[] samples = new float[1024]; 
+		AudioListener.GetSpectrumData (samples, 0, FFTWindow.Hamming);
+
+		for (int i = 0; i < numberOfObjects; i++) 
+		{
+			Vector3 previousScale = cubes[i].gameObject.transform.localScale;
+			previousScale.y = Mathf.Lerp (previousScale.y, samples[i] * spectrumScale, Time.deltaTime);
+			cubes[i].transform.localScale = previousScale;
+		}
+	}
+}
